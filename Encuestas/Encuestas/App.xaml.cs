e1f19e0601d4cf68f489e57e42cb98575fa34b1b
Thioalejo <alejo.saraza@gmail.com>
2019-06-21 22:52:32 -0500

    Se agrega primera pagina SurveysView y se configuro para que sea la pagina de inicio

--- Encuestas/Encuestas/App.xaml.cs
+++ Encuestas/Encuestas/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Encuestas.Core;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace Encuestas
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new SurveysView());
         }
 
         protected override void OnStart()
---
+++ Encuestas/Encuestas/SurveysDetailsView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Encuestas.Core
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SurveysDetailsView : ContentPage
+    {
+        public SurveysDetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
